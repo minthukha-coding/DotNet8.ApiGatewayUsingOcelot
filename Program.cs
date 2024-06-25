5ef73815870ee7c070b22bb10e65f81320c8ae57
minthukha <minthukha1239788@gmail.com>
2024-06-25 15:52:11 +0630

    [ InProgress ] - Ocelot

--- Program.cs
+++ Program.cs
@@ -1,43 +1,37 @@
-
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-namespace DotNet8.ApiGatewayUsingOcelot
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("ocelot.routes.json", optional: false, reloadOnChange: true);
+//builder.Configuration.AddJsonFile("ocelot.articles.json", optional: false, reloadOnChange: true);
+//builder.Configuration.AddJsonFile("ocelot.writers.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Configuration.AddJsonFile("ocelet.json", optional: false, reloadOnChange: true);
-            builder.Services.AddOcelot();
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            await app.UseOcelot();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.UseOcelot();
+
+app.Run();
